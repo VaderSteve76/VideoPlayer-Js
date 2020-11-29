b6c201b9e692db7b0cb7065235ca6fb28a1e6cb6
Steve <alverson.s@yahoo.com>
2020-11-29 18:30:24 -0500

    Clicking on progress bar changes time and duration

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ function updateProgress() {
   duration.textContent = `${displayTime(video.duration)}`;
 }
 
+// Click to seek within video
+function setProgress(e) {
+  newTime = e.offsetX / progressRange.offsetWidth;
+  progressBar.style.width = `${newTime * 100}%`;
+  video.currentTime = newTime * video.duration;
+}
+
 // Volume Controls
 
 
@@ -61,3 +68,4 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('ended', showPlayIcon);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
