544be5e12c6ca585b9c5830ae640230013da1943
Steve <alverson.s@yahoo.com>
2020-11-30 15:33:25 -0500

    Added playback rate functionality

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
 const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
+const speed = document.querySelector('.player-speed');
 
 
 
@@ -97,7 +98,9 @@ function toggleMute() {
 }
 
 // Change Playback Speed
-
+function changeSpeed() {
+  video.playbackRate = speed.nodeValue;
+}
 
 // Fullscreen
 
@@ -111,3 +114,4 @@ video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
 volumeIcon.addEventListener('click', toggleMute);
+speed.addEventListener('change', changeSpeed);
